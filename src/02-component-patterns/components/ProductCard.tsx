c032bdaaa5cb493c79c97c0d4bec4b818910d618
Juanjo Fernandez <juanjofb86@gmail.com>
2021-12-29 18:57:06 +0100

    Crear la shooping page y el product card

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -2,30 +2,107 @@ import { useProduct } from '../hooks/useProduct';
 
 import styles from '../styles/styles.module.css'
 import  noImage from "../assets/no-image.jpg";
+import { createContext, ReactElement, useContext } from 'react';
 
 
+interface Props {
+    product: Product;
+    children?: ReactElement | ReactElement[]
+}
 
-export const ProductCard = () => {
+interface Product {
+    id: string;
+    title: string;
+    img?: string
+}
+
+interface ProductContextProps {
+    counter: number;
+    increaseBy: ( value: number ) => void;
+    product: Product;
+}
+
+const ProductContext = createContext({} as ProductContextProps);
+const { Provider } = ProductContext
+
+
+export const ProductImage = ({img = ''}) => {
+
+    const { product } = useContext(ProductContext);
+    let imgToShow: string;
+    if( img ) {
+        imgToShow = img;
+    }else if( product.img ){
+        imgToShow = product.img;
+    }
+    else{
+        imgToShow = noImage
+    }
+
+    return (
+        <img src={ imgToShow } alt="Product" className={ styles.productImg} />
+    )
+}
+
+export const ProductTitle = ( { title }: {title?: string}) => {
+    const { product } = useContext(ProductContext);
+
+    return(
+        <span className={styles.productDescription}>
+            {title ? title : product.title}
+        </span>
+    )
+}
+
+
+export const ProductButtons = () => {
+
+
+    const { increaseBy, counter} = useContext(ProductContext)
+
+    return(
+        <div className={styles.buttonsContainer}>
+            <button
+                className={ styles.buttonMinus}
+                onClick={ () => increaseBy( -1 )}>-</button>
+            <div className={styles.countLabel}> {counter} </div>
+            <button
+                className={ styles.buttonAdd }
+                onClick={ () => increaseBy( +1 )} > + </button>
+        </div>
+    )
+}
+
+
+export const ProductCard = ({ children, product }: Props) => {
 
 
     const { counter, increaseBy } = useProduct()
 
     return (
+        <Provider value={{
+            counter,
+            increaseBy,
+            product
+        }}>
+
         <div className={ styles.productCard }>
-            <img src="./coffee-mug.png" alt=" Coffee mug" className={ styles.productImg} />
-            {/* <img src={noImage} alt=" Coffee mug" className={ styles.productImg} /> */}
 
-            <span className={styles.productDescription}>Coffee Mug</span>
+            {children}
+            {/* <ProductImage img={ product.img } />
 
-            <div className={styles.buttonsContainer}>
-                <button 
-                    className={ styles.buttonMinus}
-                    onClick={ () => increaseBy( -1 )}>-</button>
-                <div className={styles.countLabel}> {counter} </div>
-                <button 
-                    className={ styles.buttonAdd }
-                    onClick={ () => increaseBy( +1 )} > + </button>
-            </div>
+            <ProductTitle title={ product.title } />
+
+            <ProductButtons
+                counter={counter}
+                increaseBy={ increaseBy }
+                /> */}
+
         </div>
+        </Provider>
     )
 }
+
+ProductCard.Title = ProductTitle;
+ProductCard.Image = ProductImage;
+ProductCard.Buttons = ProductButtons;
--- src/02-component-patterns/pages/ShoopingPage.tsx
+++ src/02-component-patterns/pages/ShoopingPage.tsx
@@ -1,10 +1,30 @@
-import { ProductCard } from "../components/ProductCard"
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components/ProductCard"
 
-export const ShoopingPage = () => {
-    return (
-        <div>
-            <h1>Shooping store</h1>
-            <ProductCard />
-        </div>
-    )
+const product = {
+    id:'1',
+    title: 'Coffee Mug',
+    img: './coffee-mug.png',
 }
+
+export const ShoopingPage = () => (
+    <div>
+        <h1>Shooping store</h1>
+        <div style={{
+            display: 'flex',
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+        }}>
+
+            <ProductCard product={product}>
+                <ProductCard.Image />
+                <ProductCard.Title  />
+                <ProductCard.Buttons />
+            </ProductCard>
+            <ProductCard product={product}>
+                <ProductImage />
+                <ProductTitle title={'titl'} />
+                <ProductButtons />
+            </ProductCard>
+        </div>
+    </div>
+)
